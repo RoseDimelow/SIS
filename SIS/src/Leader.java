f3a5ad0ec343e80799f993ff38c9cc85b9a6f3bf
rose.dimelow <rose.dimelow@MU-C1-22.mullen.pvt.k12.co.us>
2025-03-05 13:44:50 -0700

    hiiii

--- SIS/src/Leader.java
+++ SIS/src/Leader.java
@@ -1,10 +1,10 @@
 
-public class Main {
+public class Leader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("heyyy");
 
-		System.out.println("heyyyyy");
 	}
 
 }
