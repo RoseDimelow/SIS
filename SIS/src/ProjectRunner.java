2cb0f583196f0a3aae444bbae3a1e0eefeb83d68
rose.dimelow <rose.dimelow@MU-C1-22.mullen.pvt.k12.co.us>
2025-03-11 13:24:45 -0600

    THE MOST up-to date commit as of March 11, 1:24 pm

--- SIS/src/ProjectRunner.java
+++ SIS/src/ProjectRunner.java
@@ -1,9 +1,18 @@
 
-public class ProjectRunner {
+public class ProjectRunner 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+		
+		//Hey everyone! If you are reading this right now, I think that we should be up to date. 
+		//I created a new class for each option of the first menu for the project
+		//here are the jobs we agreed to do:
+		//Aisley: work on the sort students menu option
+		//Madeline: make the main menu- you might want to make your own class for this part instead of putting it all in the project runner class
+		//Thanh-Tam: work on the add/delete option
+		//Rose: create the pojo and import the text file
 	}
 
 }
