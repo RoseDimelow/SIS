b093aa222791eccf56c2ed2a51189114d717b05e
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-03-13 13:22:13 -0600

    woop

--- SIS/src/ProjectRunner.java
+++ SIS/src/ProjectRunner.java
@@ -1,29 +1,25 @@
-<<<<<<< HEAD
+
 import java.util.Scanner;
-=======
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.ArrayList;
->>>>>>> branch 'master' of https://github.com/MadelynMeisner/SISProject.git
+
 
 public class ProjectRunner 
 {
-	static String playerGuess = userInput.nextLine(); 
+	
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
+
 		//class.method
-		System.out.println("");
+		//System.out.println("");
 		greetUser();
 		addOrDelete();
 		changeStudents();
 		sortStudents();
-=======
-
 		readTextFile();
 		
->>>>>>> branch 'master' of https://github.com/MadelynMeisner/SISProject.git
+
 		//Hey everyone! If you are reading this right now, I think that we should be up to date. 
 		//I created a new class for each option of the first menu for the project
 		//here are the jobs we agreed to do:
@@ -32,17 +28,16 @@ public class ProjectRunner
 		//Thanh-Tam: work on the add/delete option
 		//Rose: create the pojo and import the text file
 	}
-<<<<<<< HEAD
+
 	 public static void greetUser()
 	 {
 		Scanner userInput = new Scanner (System.in);
 		System.out.println("Hello! I hope you have had a splendid day!");
-		 playerGuess = userInput.nextLine(); 
 		System.out.println("What would you like to do?");
 		System.out.println("a) add or delete a student");
 		System.out.println("b) change student grades/schedule");
 		System.out.println("c) sort students");
-	    playerGuess = userInput.nextLine(); 
+	    String playerGuess = userInput.nextLine(); 
 	    // sayas a b or c 
 //		What would you like to do?
 //		add or delete a student - need add/delete methods and classes
@@ -67,10 +62,6 @@ public class ProjectRunner
 	 {
 		 
 	 }
-	
-=======
-	
-	
 	
 	
 	
@@ -102,6 +93,6 @@ public class ProjectRunner
 			System.out.println("Sorry, an error occurred.");
 		}
 	}
->>>>>>> branch 'master' of https://github.com/MadelynMeisner/SISProject.git
+
 
 }
