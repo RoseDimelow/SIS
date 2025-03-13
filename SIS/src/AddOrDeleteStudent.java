803a3081b9f0728224fd8cc8dfe57a5b002b4c88
thanhtam.nguyen <thanhtam.nguyen@MU-C1-08.mullen.pvt.k12.co.us>
2025-03-13 12:22:21 -0600

    asdflk;jsda;lf

---
+++ SIS/src/AddOrDeleteStudent.java
@@ -0,0 +1,15 @@
+
+public class AddOrDeleteStudent
+	{
+		public void addStudent()
+		{
+			// declare a scanner variable for student name and instantiate to whatever the user inputs
+			// need first name, last name, GPA (this will need to be calculated), and three courses which include the period, class name and current letter grade.
+			// adds a student 
+		}
+		
+		public void deleteStudent()
+		{
+			
+		}
+	}
