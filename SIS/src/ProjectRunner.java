e1024997e6cf98815f940e5f67fe0c9bf91991d0
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-03-13 13:12:13 -0600

    addeed some stuff in the main method

--- SIS/src/ProjectRunner.java
+++ SIS/src/ProjectRunner.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 
 public class ProjectRunner 
 {
-
+	static String playerGuess = userInput.nextLine(); 
 	public static void main(String[] args) 
 	{
-
-		
+		//class.method
+		System.out.println("");
+		greetUser();
+		addOrDelete();
+		changeStudents();
+		sortStudents();
 		//Hey everyone! If you are reading this right now, I think that we should be up to date. 
 		//I created a new class for each option of the first menu for the project
 		//here are the jobs we agreed to do:
@@ -14,5 +19,40 @@ public class ProjectRunner
 		//Thanh-Tam: work on the add/delete option
 		//Rose: create the pojo and import the text file
 	}
+	 public static void greetUser()
+	 {
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Hello! I hope you have had a splendid day!");
+		 playerGuess = userInput.nextLine(); 
+		System.out.println("What would you like to do?");
+		System.out.println("a) add or delete a student");
+		System.out.println("b) change student grades/schedule");
+		System.out.println("c) sort students");
+	    playerGuess = userInput.nextLine(); 
+	    // sayas a b or c 
+//		What would you like to do?
+//		add or delete a student - need add/delete methods and classes
+//		addStudent()
+//		deleteStudent()
+//		change student grades/schedule - need a class & methods
+//		changeStudentGrades()
+//		changeStudentSchedule()
+//		sort students - need a class & methods
+//		sortStudents()
+
+	 }
+	 public static void addOrDelete()
+	 {
+		 
+	 }
+	 public static void changeStudents()
+	 {
+		 
+	 }
+	 public static void sortStudents()
+	 {
+		 
+	 }
+	
 
 }
