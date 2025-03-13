d74ed0e5cd049bf450bbc040fa7f7ad6591d8d99
rose.dimelow <rose.dimelow@MU-C1-22.mullen.pvt.k12.co.us>
2025-03-13 12:37:20 -0600

    Trying to see if my branch will show up in github

--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -1,4 +1,45 @@
 
-public class Student {
+public class Student 
+{
+	private String firstName;
+	private String lastName;
+	private double gpa;
+	//add class periods
+	
+	public Student(String f, String l, double g)
+	{
+		firstName = f;
+		lastName = l; 
+		gpa = g;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
 
+	public void setFirstName()
+	{
+		this.firstName = firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public void setLastName()
+	{
+		this.lastName = lastName;
+	}
+	
+	public double getGpa()
+	{
+		return gpa;
+	}
+	
+	public void setGpa()
+	{
+		this.gpa = gpa;
+	}
 }
