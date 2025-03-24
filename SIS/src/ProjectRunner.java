e377052076cdaab11fa50ded7e971d213066c810
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-03-24 10:14:37 -0600

    just awdded some if statemtbs in dirgver class

--- SIS/src/ProjectRunner.java
+++ SIS/src/ProjectRunner.java
@@ -20,13 +20,7 @@ public class ProjectRunner
 		readTextFile();
 		
 
-		//Hey everyone! If you are reading this right now, I think that we should be up to date. 
-		//I created a new class for each option of the first menu for the project
-		//here are the jobs we agreed to do:
-		//Aisley: work on the sort students menu option
-		//Madeline: make the main menu- you might want to make your own class for this part instead of putting it all in the project runner class
-		//Thanh-Tam: work on the add/delete option
-		//Rose: create the pojo and import the text file
+		
 	}
 
 	 public static void greetUser()
@@ -38,6 +32,18 @@ public class ProjectRunner
 		System.out.println("b) change student grades/schedule");
 		System.out.println("c) sort students");
 	    String playerGuess = userInput.nextLine(); 
+	    if(playerGuess.equals("a") || playerGuess.equals("a)"))
+	    	{
+	    		addOrDelete();
+	    	}
+	    if(playerGuess.equals("b") || playerGuess.equals("b)"))
+	    	{
+	    		changeStudents();
+	    	}
+	    if(playerGuess.equals("c") || playerGuess.equals("c)"))
+	    	{
+	    		sortStudents();
+	    	}
 	    // sayas a b or c 
 //		What would you like to do?
 //		add or delete a student - need add/delete methods and classes
@@ -52,7 +58,7 @@ public class ProjectRunner
 	 }
 	 public static void addOrDelete()
 	 {
-		 
+		
 	 }
 	 public static void changeStudents()
 	 {
@@ -60,7 +66,7 @@ public class ProjectRunner
 	 }
 	 public static void sortStudents()
 	 {
-		 
+		 SortStudents.sortLastName();
 	 }
 	
 	
