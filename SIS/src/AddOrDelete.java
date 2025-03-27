6f15db7a692cbddd5580f501dc9910ce562bf94b
thanhtam.nguyen <thanhtam.nguyen@MU-C1-08.mullen.pvt.k12.co.us>
2025-03-27 12:37:07 -0600

    completed the add or delete method

--- SIS/src/AddOrDelete.java
+++ SIS/src/AddOrDelete.java
@@ -1,16 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddOrDelete
 	{
-
-		public static void main(String[] args)
+		
+		public static void addStudent() throws FileNotFoundException
 			{
 				Scanner userInput = new Scanner(System.in);
 				String defaultGrade = "[--]";
-				String defaultGPA = "0.0";
+				double defaultGPA = 0.0;
 
 				System.out.print("Student Name: ");
-				String firstName = userInput.nextLine();
+				String firstName = userInput.nextLine(); // Student parameter
 
 				System.out.print("Student Last Name: ");
 				String lastName = userInput.nextLine();
@@ -24,26 +27,28 @@ public class AddOrDelete
 				System.out.print("Period 3 Class: ");
 				String periodThreeClass = userInput.nextLine();
 
-				String[] newStudent = {firstName, lastName, periodOneClass, defaultGrade, periodTwoClass, defaultGrade, periodThreeClass, defaultGrade, defaultGPA};
-				System.out.println("Student Succesfully Added: " + firstName + " " + lastName + " " + periodOneClass + " " + periodTwoClass + " " + periodTwoClass);
+				Student student = new Student(firstName, lastName, periodOneClass, defaultGrade, periodTwoClass,
+						defaultGrade, periodThreeClass, defaultGrade, defaultGPA);
 
-				// GPA default = 0
-				// Grade default = "[--]"
+				ArrayList<Student> studentList = new ArrayList<Student>();
+
+				studentList.add(student);
+
+				System.out.println("Student Succesfully Added: " + firstName + " " + lastName + " " + periodOneClass
+						+ " " + defaultGrade + " " + periodTwoClass + " " + defaultGrade + " " + periodThreeClass + " "
+						+ defaultGrade + " " + defaultGPA);
+
+				Scanner myFile = new Scanner(new File("studentList.txt"));
+
+				while (myFile.hasNext())
+					{
+						studentList.add(new Student(myFile.next(), myFile.next(), myFile.next(), myFile.next(),
+								myFile.next(), myFile.next(), myFile.next(), myFile.next(), 0.0));
+					}
+
 			}
 
-		public void addStudent()
-			{
-				Scanner userInput = new Scanner(System.in);
-				System.out.print("Student Name: ");
-				System.out.println(userInput);
-				// declare a scanner variable for student name and instantiate to whatever the
-				// user inputs
-				// need first name, last name, GPA (this will need to be calculated), and three
-				// courses which include the period, class name and current letter grade.
-				// adds a student
-			}
-
-		public void deleteStudent()
+		public static void deleteStudent() throws FileNotFoundException
 			{
 				Scanner userInput = new Scanner(System.in);
 
@@ -53,5 +58,26 @@ public class AddOrDelete
 				System.out.print("Student Last Name: ");
 				String lastName = userInput.nextLine();
 
+				ArrayList<Student> studentList = new ArrayList<Student>();
+
+				Scanner myFile = new Scanner(new File("studentList.txt"));
+
+				while (myFile.hasNext())
+					{
+						studentList.add(new Student(myFile.next(), myFile.next(), myFile.next(), myFile.next(),
+								myFile.next(), myFile.next(), myFile.next(), myFile.next(), 0.0));
+					}
+
+				for (int i = 0; i < studentList.size(); i++)
+					{
+						if (studentList.get(i).getFirstName().equals(firstName)
+								&& studentList.get(i).getLastName().equals(lastName))
+							{
+								studentList.remove(studentList.get(i));
+							}
+					}
+
+				System.out.println(firstName + " " + lastName + " successfully removed.");
 			}
+
 	}
--- SIS/src/SortStudents.java
+++ SIS/src/SortStudents.java
@@ -1,4 +1,46 @@
+import java.util.Scanner;
 
-public class SortStudents {
-
+public class SortStudents 
+{	
+	public static void questions()
+	{
+		Scanner userIntInput = new Scanner(System.in);
+		
+		System.out.println("How would you like to organize the students?");
+		System.out.println("1. Last Name");
+		System.out.println("2. GPA");
+		System.out.println("3. Period");
+		
+		int answer = userIntInput.nextInt();
+		
+		if (answer == 1 || answer == 1.)
+			{
+				sortLastName();
+			}
+		
+		else if (answer == 2 || answer == 2.)
+			{
+				sortGPA();
+			}
+		
+		else
+			{
+				sortPeriod();
+			}
+	}
+	
+	public static void sortLastName()
+	{
+		
+	}
+	
+	public static void sortGPA()
+	{
+		
+	}
+	
+	public static void sortPeriod()
+	{
+		
+	}
 }
