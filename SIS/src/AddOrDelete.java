0383479f469520b640ff6ae0e927d4292df333ad
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-03-27 12:34:23 -0600

    commit

--- SIS/src/AddOrDelete.java
+++ SIS/src/AddOrDelete.java
@@ -41,17 +41,17 @@ public class AddOrDelete
 				Scanner myFile = new Scanner (new File("studentList.txt"));
 			}
 
-		public static void addStudent()
-			{
-				Scanner userInput = new Scanner(System.in);
-				System.out.print("Student Name: ");
-				System.out.println(userInput);
-				// declare a scanner variable for student name and instantiate to whatever the
-				// user inputs
-				// need first name, last name, GPA (this will need to be calculated), and three
-				// courses which include the period, class name and current letter grade.
-				// adds a student
-			}
+//		public static void addStudent()
+//			{
+//				Scanner userInput = new Scanner(System.in);
+//				System.out.print("Student Name: ");
+//				System.out.println(userInput);
+//				// declare a scanner variable for student name and instantiate to whatever the
+//				// user inputs
+//				// need first name, last name, GPA (this will need to be calculated), and three
+//				// courses which include the period, class name and current letter grade.
+//				// adds a student
+//			}
 
 		public static void deleteStudent()
 			{
